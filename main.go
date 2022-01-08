12e68e8e290aebe6db6b54a72c0333ea89574c86
Rohan Allison <rohan.allison@cbre.com>
2022-01-07 21:30:52 -0600

    Sruthi's app

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
@@ -11,11 +12,21 @@ import (
 
 func main() {
 	myApp := app.New()
-	w := myApp.NewWindow("Text")
+	w := myApp.NewWindow("Sruthi's App")
+	w.Resize(fyne.Size{
+		Width:  500,
+		Height: 500,
+	})
 
-	top := canvas.NewText("top bar", color.White)
+	top := canvas.NewText("top bar", color.RGBA{
+		R: 250,
+		G: 20,
+		B: 250,
+		A: 0,
+	})
 	left := canvas.NewText("left", color.White)
 	middle := canvas.NewText("content", color.White)
+
 	content := container.New(layout.NewBorderLayout(top, nil, left, nil),
 		top, left, middle)
 	w.SetContent(content)
