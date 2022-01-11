0982cb6332d7a143278d473238b5c0c9322e0666
Rohan Allison <rohan.allison@cbre.com>
2022-01-11 13:57:46 -0600

    Exercising layouts with a calculator

--- main.go
+++ main.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
 	w := myApp.NewWindow("Entry Widget")
+	w.Resize(fyne.Size{Width: 500, Height: 500})
 
-	w.Resize(fyne.Size{
-		Width:  500,
-		Height: 500,
-	})
-
+	// Entry
 	lbl := widget.NewLabel("Text Entry")
-	in := widget.NewEntry()
-	in.SetPlaceHolder("Enter text...")
+	in := widget.NewEntry() // Text input
+	in.SetPlaceHolder("Enter two decimal numbers...")
+
+	result := widget.NewEntry()
+
 	saveBtn := widget.NewButton("Save",
-		func() {println("You entered:", in.Text)})
+		func() {
+			// Parse entry
+			var a, b float64
+			_, err := fmt.Sscanf(in.Text, "%f %f", &a, &b)
+			if err != nil {
+				log.Println("Error reading the input", err.Error())
+				return
+			}
+			// Return the result
+			sum := a + b
+			result.SetText("> " + fmt.Sprintf("%0.1f", sum))
+		})
 
-	content := container.NewVBox(lbl, in, saveBtn)
+	con := container.NewVBox(
+		lbl, in, widget.NewSeparator(),
+		layout.NewSpacer(),
+		saveBtn, widget.NewSeparator(), result)
 
-	w.SetContent(content)
+	w.SetContent(con)
 	w.ShowAndRun()
 }
